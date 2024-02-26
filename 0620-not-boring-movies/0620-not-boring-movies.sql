ee11eec0791906bacd993d428782aa10c45b5554
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-02-27 03:20:21 +0900

    Update 0620-not-boring-movies.sql

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,2 +1,4 @@
 /* Write your PL/SQL query statement below */
 select * from Cinema having mod(id, 2)!=0 and description != 'boring' group by id, movie, description, rating order by rating desc
+
+/*집계함수는 group by 필수 (select절에 집계함수만 있을 시 생략가능. 이 경우엔 전체 row가 하나의 그룹화*/
