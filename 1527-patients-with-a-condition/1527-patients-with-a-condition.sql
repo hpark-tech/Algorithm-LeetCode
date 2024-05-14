268295c47dcca0d3e15e4ee17c7935a3e000c839
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-05-14 23:50:50 +0900

    Update 1527-patients-with-a-condition.sql

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,3 +1,3 @@
 SELECT patient_id, patient_name, conditions
 FROM Patients
-WHERE FIND_IN_SET('DIAB1', conditions) > 0 OR conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%' OR conditions LIKE '% DIAB1';
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%' OR conditions LIKE 'DIAB1% %';
