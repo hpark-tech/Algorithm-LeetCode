c09265ad3676e069233d0a7c8fd876a55f09bd29
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-05-21 06:46:45 +0900

    Update 0570-managers-with-at-least-5-direct-reports.sql

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,1 +1,2 @@
 select name from Employee where id in (select managerId from (select managerId, count(managerId) from Employee group by managerId having count(managerId)>=5) as t1)
+-- select name from Employee e1 join (select managerId, count(managerId) from Employee group by managerId having count(managerId)>=5) e2 on e1.id=e2.managerId
