37e56113381653b0055186ffbc55424b6770a26f
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-05-23 23:05:44 +0900

    Update 1070-product-sales-analysis-iii.sql

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,2 +1,1 @@
-
-select s1.product_id, first_year, quantity, price from Sales s1 join (select product_id, min(year) as first_year from Sales group by product_id) as s2 on s1.product_id = s2.product_id and s1.year = s2.first_year;
+select s1.product_id, first_year, quantity, price from Sales s1 join (select product_id, min(year) as first_year from Sales group by product_id) as s2 on s1.product_id = s2.product_id where s1.year = s2.first_year
