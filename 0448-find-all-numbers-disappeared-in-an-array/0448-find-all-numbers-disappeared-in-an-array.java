c028816c520fd37fcd7507072d5eafff690c3452
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-06-22 13:26:55 +0900

    Time: 3 ms (99.14%), Space: 54.8 MB (41.92%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-    for(int i=1; i<=nums.length; i++){set.add(i);}
-    for(int k : nums){if(set.contains(k)){set.remove(k);}}
-    ArrayList<Integer> list = new ArrayList<>(set);
-    return list;
+        ArrayList<Integer> list = new ArrayList<>();
+        int result[] = new int[nums.length+1];
+        for(int i=0; i<nums.length; i++){result[nums[i]]++;}
+        for(int i=0; i<nums.length+1; i++){if(result[i]==0&&i!=0){list.add(Integer.valueOf(i));}}
+        return list;
     }
 }
 
@@ -14,3 +14,15 @@ public List<Integer> findDisappearedNumbers(int[] nums) {
     for(int i=1; i<=nums.length; i++){list.add(i);}
     for(int k : nums){if(list.contains(k)){list.remove(Integer.valueOf(k));}}
     return list;*/
+
+    /* 22ms
+    class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+    for(int i=1; i<=nums.length; i++){set.add(i);}
+    for(int k : nums){if(set.contains(k)){set.remove(k);}}
+    ArrayList<Integer> list = new ArrayList<>(set);
+    return list;
+    }
+}
+*/
